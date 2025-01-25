bbfd0018ee84f642b448533402ba4c14ae40017d
Aron <117737809+Dolus787@users.noreply.github.com>
2025-01-25 16:33:55 +0100

    FEAT: can move slow

--- Player.gd
+++ Player.gd
@@ -38,7 +38,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction := Vector3($SpringArm3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := Vector3($SpringArm3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y))
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
