9d8ca8d630819f8c6a52993f12cc789764108f26
tage <tagetjerngren@gmail.com>
2025-01-25 12:01:26 +0100

    Feat: Added a light attack display and tweaked camera

--- Player.gd
+++ Player.gd
@@ -6,12 +6,14 @@ var camera:Camera3D
 @export
 var springArm:SpringArm3D
 
-const camMin = -0.3
+const camMin = -0.7
 const camMax = 0.7
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var lastMouse:Vector2
+
+var bAttacking = false
 
 func _ready() -> void:
 	#Input.mouse_mode = Input.MouseMode.MOUSE_MODE_CAPTURED;
@@ -32,10 +34,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := Vector3($SpringArm3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		$Body.look_at(position - Vector3(direction.x, 0, direction.z))
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -44,14 +48,28 @@ func _physics_process(delta: float) -> void:
 
 	var deltaMouse:Vector2 = get_viewport().get_mouse_position()-lastMouse
 	
-	if camera.rotation.x-deltaMouse.y/150>camMin and camera.rotation.x-deltaMouse.y/150<camMax:
-		camera.rotation.x += -deltaMouse.y/150
+	if springArm.rotation.x-deltaMouse.y/150>camMin and springArm.rotation.x-deltaMouse.y/150<camMax:
+		#camera.rotation.x += -deltaMouse.y/150
 		springArm.rotation.x += -deltaMouse.y/150
 		
 	print(camera.rotation.x)
-	rotation.y += -deltaMouse.x/150
+	#rotation.y += -deltaMouse.x/150
 	#camera.rotate(Vector3(1,0,0), -deltaMouse.y/150)
 	#springArm.rotate(Vector3(1,0,0), -deltaMouse.y/150)
+	springArm.rotation.y += -deltaMouse.x/150
+	
+	if Input.is_action_just_pressed("light_attack"):
+		bAttacking = true
+		$Body/WeaponRoot/WeaponModel.show()
+		$Body/WeaponRoot.rotation = Vector3(0, -PI/2, 0)
+		
+	if bAttacking:
+		$Body/WeaponRoot.rotate_y(0.2)
+		if $Body/WeaponRoot.rotation.y > PI/2:
+			bAttacking = true
+			$Body/WeaponRoot/WeaponModel.hide()
+			
+		
 	
 	lastMouse = get_viewport().get_mouse_position()
 	
