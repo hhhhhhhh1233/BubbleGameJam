5c9ec23805b1349966309b5fc9d3c7a6763b763a
HodurGustavs <hodurgustavsson@gmail.com>
2025-01-25 16:21:51 +0100

    feat: rat
    Added the model and animations to the character rat

--- Player.gd
+++ Player.gd
@@ -30,6 +30,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$Body/AnimationTree["parameters/jump/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
