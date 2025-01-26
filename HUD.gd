820400bc49b8ae42e066d474cf92826fca6bd586
Aron <117737809+Dolus787@users.noreply.github.com>
2025-01-26 10:05:11 +0100

    Further death screen updates

--- HUD.gd
+++ HUD.gd
@@ -15,10 +15,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Time.get_unix_time_from_system()-start>5:
-		info.visible = false
-	health.text = "Health:" + str(healthComponent.health) + "%"
-	currentWave.text = "Current wave: " + str(WaveInformation.WaveCount)
 	if healthComponent.health <= 0:
 		urDeadBro.text = "You died\nYou got to wave " + str(WaveInformation.WaveCount) + "\nPress A/X/Enter to restart"
+	else:
+		if Time.get_unix_time_from_system()-start>5:
+			info.visible = false
+		health.text = "Health:" + str(healthComponent.health) + "%"
+		currentWave.text = "Current wave: " + str(WaveInformation.WaveCount)
 	pass
